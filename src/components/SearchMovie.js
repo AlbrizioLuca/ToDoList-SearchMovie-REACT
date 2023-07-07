d99ffd4296f5b99d574780ac548fc41235143bfb
AlbrizioLuca <luca_albrizio@hotmail.com>
2023-07-07 16:42:34 +0200

    Correction pour code Movie en approche fonctionnelle

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,44 +1,40 @@
 import React, { useState } from 'react';
 import MovieDetails from './MovieDetails';
 
-function SearchMovies() {
-    const [title, setTitle] = useState(''); // Déclare une variable d'état pour le titre du film
-    const [movies, setMovies] = useState([]); // Déclare une variable d'état pour les films trouvés
-    const [selectedMovie, setSelectedMovie] = useState(null); // Déclare une variable d'état pour le film sélectionné
+const SearchMovies = () => {
+    const [title, setTitle] = useState('');
+    const [movies, setMovies] = useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
-    // Fonction pour chercher les films par titre
     const searchMoviesByTitle = async (event) => {
-        event.preventDefault(); // Empêche la page de se recharger lors de la soumission du formulaire
+        event.preventDefault();
 
-        const url = `http://www.omdbapi.com/?s=${title}&apikey=82419599`; // URL de l'API OMDB pour chercher les films par titre
+        const url = `http://www.omdbapi.com/?s=${title}&apikey=82419599`;
 
         try {
-            const res = await fetch(url); // Envoie une requête à l'API
-            const data = await res.json(); // Convertit la réponse en JSON
-            setMovies(data.Search); // Met à jour la variable d'état des films trouvés
+            const res = await fetch(url);
+            const data = await res.json();
+            setMovies(data.Search);
         } catch (err) {
-            console.error(err); // Affiche une erreur s'il y a un problème avec la requête
+            console.error(err);
         }
     }
 
-    // Fonction pour charger les détails d'un film
     const loadMovieDetails = async (id) => {
-        const url = `http://www.omdbapi.com/?i=${id}&apikey=82419599`; // URL de l'API OMDB pour chercher les détails d'un film
+        const url = `http://www.omdbapi.com/?i=${id}&apikey=82419599`;
         try {
-            const res = await fetch(url); // Envoie une requête à l'API
-            const data = await res.json(); // Convertit la réponse en JSON
-            setSelectedMovie(data); // Met à jour la variable d'état du film sélectionné
+            const res = await fetch(url);
+            const data = await res.json();
+            setSelectedMovie(data);
         } catch (err) {
-            console.error(err); // Affiche une erreur s'il y a un problème avec la requête
+            console.error(err);
         }
     }
 
-    // Si un film est sélectionné, affiche les détails du film
     if (selectedMovie) {
         return <MovieDetails movie={selectedMovie} />
     }
 
-    // Sinon, affiche le formulaire de recherche et les résultats de la recherche
     return (
         <div>
             <form onSubmit={searchMoviesByTitle}>
